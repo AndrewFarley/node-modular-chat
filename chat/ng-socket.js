b8d2c25f50dfd15052a94641de3ec4fc58da302b
Kristian <kristian.boetcher@agillic.com>
2015-02-12 16:53:24 +0100

    Made a working angular prototype with connection count and message display.

--- chat/ng-socket.js
+++ chat/ng-socket.js
@@ -1,17 +1,32 @@
-var MyApp = angular.module('MyApp', []);
-angular.module('MyApp').factory('MyService', ['$q', '$rootScope', function($q, $rootScope) {
-    // We return this object to anything injecting our service
-    var Service = {};
-    // Keep all pending requests here until they get responses
-    var callbacks = {};
-    // Create a unique callback ID to map requests to responses
+var sockItToMe = angular.module('sockItToMe', []);
+sockItToMe.factory('socket', ['$q', '$rootScope', function($q, $rootScope) {
+    /*
+     * Stores all the data sent from the websocket server
+     */
+    var entryData = {
+        "postMessage": []
+        , "setPeerCount": {}
+        , "nudge": {}
+    };
+    /*
+     * Access control object for websocket messages
+     */
+    var type = {
+        "add": {
+            "postMessage": true
+        }
+        , "set": {
+            "setPeerCount": true
+            , "nudge": true
+        }
+    };
+    /*
+     * Callback ID used to track amount of messages sent
+     */
     var currentCallbackId = 0;
-    // Create our websocket object with the address to the websocket
-    var ws = new WebSocket("ws://nexination.com:8080/socket/");
-    
-          /*
-        Cleans JSON RPC 2.0 objects for new use.
-    */
+    /*
+     * Cleans JSON RPC 2.0 objects for new use.
+     */
     var getCleanRpcObject = function(objectType) {
         currentCallbackId += 1;
         var rpc2 = {"jsonrpc": "2.0", "id": currentCallbackId};
@@ -27,54 +42,44 @@ angular.module('MyApp').factory('MyService', ['$q', '$rootScope', function($q, $
         
         return rpc2;
     };
+    /*
+     * Sets up the data handler for use on incoming websocket messages
+     */
+    var dataProcessor = function(rpc2) {
+        console.log(rpc2);
+        if(rpc2.hasOwnProperty("method") && entryData.hasOwnProperty(rpc2.method)) {
+            if(type.set[rpc2.method] !== undefined) {
+                $rootScope.$apply(function() {
+                    entryData[rpc2.method] = rpc2.params;
+                });
+                $rootScope.$apply();
+            }
+            else if(type.add[rpc2.method] !== undefined) {
+                $rootScope.$apply(function() {
+                    entryData[rpc2.method].unshift(rpc2.params);
+                });
+            };
+        };
+    };
     
+    /*
+     * Initiates a websocket connection and sets up all the necessary message handlers
+     */
+    var ws = new WebSocket("ws://nexination.com:8080/socket/");
     ws.onopen = function(){  
         console.log("Socket has been opened!");  
     };
     
     ws.onmessage = function(message) {
-        listener(JSON.parse(message.data));
+        dataProcessor(JSON.parse(message.data));
     };
-
-    function sendRequest(request) {
-      var defer = $q.defer();
-      var callbackId = request.id;
-      callbacks[callbackId] = {
-        time: new Date(),
-        cb:defer
-      };
-      console.log('Sending request', request);
-      ws.send(JSON.stringify(request));
-      return defer.promise;
-    }
-
-    function listener(data) {
-      var messageObj = data;
-      console.log("Received data from websocket: ", messageObj);
-      // If an object exists with id in our callbacks object, resolve it
-      if(callbacks.hasOwnProperty(messageObj.id)) {
-        console.log(callbacks[messageObj.id]);
-        $rootScope.$apply(callbacks[messageObj.id].cb.resolve(messageObj.params));
-        console.log($rootScope);
-        delete callbacks[messageObj.callbackID];
-      }
-    }
-
-    // Define a "getter" for getting customer data
-    Service.getCustomers = function() {
-        var request = getCleanRpcObject('request');
-        request.method = 'postMessage';
-        request.params.name = 'DaftPink';
-        request.params.message = 'Earl of gray!';
-        // Storing in a variable for clarity on what sendRequest returns
-        var promise = sendRequest(request); 
-        return promise;
-    }
-
-    return Service;
+    
+    /*
+     * Returns the factory data object
+     */
+    return entryData;
 }]);
 
-angular.module('MyApp')
-  .controller('customerList', ['MyService', '$scope', function(MyService, $scope){
-    $scope.customers = MyService.getCustomers();
-  }]);
+sockItToMe.controller('messageController', ['$scope', 'socket', function($scope, socket){
+    $scope.socket = socket;
+}]);
