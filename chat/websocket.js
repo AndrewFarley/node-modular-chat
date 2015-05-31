850ebedba77a485ebd485296832ff6e033d40ce4
Kris B <KrisZane@users.noreply.github.com>
2015-05-31 22:01:49 +0200

    [TASK] Complete cleanup of old JS code.

--- chat/websocket.js
+++ chat/websocket.js
@@ -1,311 +1,338 @@
-var WebSock = new function() {
-    var objOutput = document.getElementById('chatMessageDisplay');
-    var objUserCount = document.getElementById('userCount');
-    var objInputField = document.getElementById('inputField');
-    
-    var objButtons = document.getElementById('buttonContainer');
-    objButtons.removeAttribute('id');
-    
-    this.objOptions = {
-        "intMessageCounter": 0
-        , "intIdCounter": 0
+var SockLib = {
+    // Initial setup variables
+    "domSelector": {
+        "chatMessageDisplay": {}
+        , "userCount": {}
+        , "inputField": {}
+        , "buttonContainer": {}
+    }
+    , "options": {
+        "messageCount": 0
+        , "jsonId": 0
         , "parameters": {}
         , "audioTags": {}
-    };
-    this.objError = {
-        "code": ""
-        , "message": ""
-        , "data": ""
-    };
-    this.objWebsocket = {};
+        , "jsonType": {
+            "main": function() {
+                return {
+                    "jsonrpc": "2.0"
+                    , "id": SockLib.options.jsonId
+                };
+            }
+            , "error": function() {
+                var rpc2 = new SockLib.options.jsonType.main;
+                rpc2.error = {
+                    "code": ""
+                    , "message": ""
+                    , "data": ""
+                };
+                return rpc2;
+            }
+            , "request": function() {
+                var rpc2 = new SockLib.options.jsonType.main;
+                rpc2.params = {};
+                rpc2.method = '';
+                return rpc2;
+            }
+            , "result": function() {
+                var rpc2 = new SockLib.options.jsonType.main;
+                rpc2.result = {};
+                return rpc2;
+            }
+        }
+        , "error": {
+            "code": ""
+            , "message": ""
+            , "data": ""
+        }
+    }
+    , "socket": {}
+    , "stringParse": [
+        {
+            "regex": /\S+(youtube)\S+v=([a-zA-Z0-9_\-]+)\S*/gim
+            , "replace": '<iframe style="youtubeFrame" width="320" height="320" src="//www.youtube.com/embed/$2" frameborder="0" allowfullscreen></iframe>'
+        }
+        , {
+            "regex": /!!(\S+)!!/gim
+            , "replace": '<img class="images" src="$1" />'
+        }
+        , {
+            "regex": /\s(http[s]?:\S+)/gim
+            , "replace": '<a href="$1" target="_blank">$1</a>'
+        }
+    ]
     
     /*
-        Creates websocket connection, as well as any automated functionality.
+        Creates WebSocket connection, as well as any automated functionality.
     */
-    this.__constructor = function() {
+    , "__constructor": function() {
+        // Loads needed dom objects into object
+        for(var dom in this.domSelector) {
+            this.domSelector[dom] = document.getElementById(dom);
+            this.domSelector[dom].removeAttribute('id');
+        };
         // Loads get parameters into object
-        var strParameters = window.location.search.substr(1);
-        var arrParameters = strParameters.split('&');
-        for(var i = 0; i < arrParameters.length; i += 1) {
-            var arrTemp = arrParameters[i].split('=');
-            this.objOptions.parameters[arrTemp[0]] = arrTemp[1];
+        var parameters =  window.location.search.substr(1).split('&');
+        for(var i = 0; i < parameters.length; i += 1) {
+            var tempString = parameters[i].split('=');
+            this.options.parameters[tempString[0]] = tempString[1];
         };
-        
-        // Sets default name, in case no get parameter with name was given
-        if(!this.objOptions.parameters.hasOwnProperty('name')) {
-            this.objOptions.parameters.name = 'McElroy';
+        // Sets default name, port and host, in case none were set through parameters
+        if(!this.options.parameters.hasOwnProperty('name')) {
+            this.options.parameters.name = 'McElroy';
         };
-        
-        // Sets default port, in case no get parameter with port was given
-        if(!this.objOptions.parameters.hasOwnProperty('port')) {
-            this.objOptions.parameters.port = '8080';
+        if(!this.options.parameters.hasOwnProperty('port')) {
+            this.options.parameters.port = '8080';
         };
-        
+        if(!this.options.parameters.hasOwnProperty('host')) {
+            this.options.parameters.host = document.location.host;
+        };
         // Finds all audio tags and arranges them in an object list for use later
-        var arrAudioList = document.getElementsByTagName('audio');
-        for(var i = 0; i < arrAudioList.length; i += 1) {
-            var objTemp = arrAudioList[i];
-            var strTemp = objTemp.getAttribute('name');
-            if(strTemp !== undefined) {
-                WebSock.objOptions.audioTags[strTemp] = objTemp;
+        var audioList = document.getElementsByTagName('audio');
+        for(var i = 0; i < audioList.length; i += 1) {
+            var tempObject = audioList[i];
+            var tempString = tempObject.getAttribute('name');
+            if(tempString !== undefined) {
+                this.options.audioTags[tempString] = tempObject;
             };
         };
-        
-        // Creates websocket connection
-        var strWebsocketUri = 'ws://' + document.location.host + ':' + this.objOptions.parameters.port + '/nexus/socket';
-        this.objWebsocket = new WebSocket(strWebsocketUri);
-        // Defines function names to use on websocket responses
-        this.objWebsocket.onerror = function(objEvent) {onError(objEvent);};
-        this.objWebsocket.onclose = function(objEvent) {onError(objEvent);};
-        this.objWebsocket.onmessage = function(objEvent) {onMessage(objEvent);};
-        this.objWebsocket.onopen = function(objEvent) {onOpen(objEvent);};
-        
+        // Opens socket connection
+        this.connectSocket();
         // Sets an a function to reset messages received when you leave the window
-        window.onblur = function() {WebSock.updateWindow(0);};
-        
+        window.onblur = function() {SockLib.updateWindow(0);};
         // Sets a keep alive function that calls it self, to stop the connection from closing
         this.keepItAlive();
-        
-        // Removes id's from dom elements, once they have been captured
-        objOutput.removeAttribute('id');
-        objUserCount.removeAttribute('id');
-        objInputField.removeAttribute('id');
-        
         // Set default volume to 10
         this.setVolume(10);
         
         return false;
-    };
+    }
+    /*
+        Initiates a socket connection, based on the options.parameters
+    */
+    , "connectSocket": function() {
+        var uri = 'ws://' + this.options.parameters.host + ':' + this.options.parameters.port + '/nexus/socket';
+        this.socket = new WebSocket(uri);
+        this.socket.onerror = function(eventObject) {SockLib.onError(eventObject);};
+        this.socket.onclose = function(eventObject) {SockLib.onError(eventObject);};
+        this.socket.onmessage = function(eventObject) {SockLib.onMessage(eventObject);};
+        this.socket.onopen = function(eventObject) {SockLib.onOpen(eventObject);};
+        return false;
+    }
     /*
         Checks if enter is hit in the input box and sends it when enter is pressed,
         this is done because it is attached to an event on the input tag.
     */
-    this.checkSendText = function(objEvent) {
-        objEvent.target = (objEvent.target ? objEvent.target : objEvent.srcElement);
-        if(objEvent.keyCode === 13) {
-            sendText(objEvent.target.value);
-            objEvent.target.value = '';
+    , "checkSendText": function(eventObject) {
+        eventObject.target = (eventObject.target ? eventObject.target : eventObject.srcElement);
+        if(eventObject.keyCode === 13) {
+            this.sendText(eventObject.target.value);
+            eventObject.target.value = '';
         };
         return false;
-    };
+    }
     /*
         Sets all messages to display none.
     */
-    this.clearMessages = function() {
-        var arrTemp = objOutput.childNodes;
-        for(var i = 0; i < arrTemp.length; i += 1) {
-            if(typeof arrTemp[i] === 'object') {
-                if(arrTemp[i].style.display === 'none') {
+    , "clearMessages": function() {
+        var messageList = this.domSelector.chatMessageDisplay.childNodes;
+        for(var i = 0; i < messageList.length; i += 1) {
+            if(typeof messageList[i] === 'object') {
+                if(messageList[i].style.display === 'none') {
                     break;
                 };
-                arrTemp[i].style.display = 'none';
+                messageList[i].style.display = 'none';
             };
         };
         return false;
-    };
+    }
     /*
         Volume adjuster
     */
-    this.setVolume = function(volumePercentage) {
+    , "setVolume": function(volumePercentage) {
         var volume = volumePercentage / 100;
-        for(var i in this.objOptions.audioTags) {
-            this.objOptions.audioTags[i].volume = volume;
+        for(var i in this.options.audioTags) {
+            this.options.audioTags[i].volume = volume;
         };
         return false;
-    };
+    }
     /*
         Detects if autoclear has been set
         and clears all messages and resets the counter.
     */
-    this.updateWindow = function(intCounterSet) {
-        
-        if(typeof intCounterSet === 'number') {
-            this.objOptions.intMessageCounter = intCounterSet;
-            
-            if(this.objOptions.parameters.hasOwnProperty('autoclear')) {
-                this.clearMessages();
-            };
+    , "updateWindow": function(counter) {
+        if(typeof counter === 'number') {
+            this.options.messageCount = counter;
         }
         else {
-            this.objOptions.intMessageCounter += 1;
+            this.options.messageCount += 1;
         };
-        
-        document.title = this.objOptions.intMessageCounter + ' new';
+        document.title = this.options.messageCount + ' new';
         
         return false;
-    };
+    }
     /*
         Plays mp3 notification.
     */
-    this.playNotifications = function(strWhatToPlay) {
-        this.objOptions.audioTags[strWhatToPlay].currentTime = 0;
-        this.objOptions.audioTags[strWhatToPlay].play();
+    , "playNotifications": function(playSelection) {
+        this.options.audioTags[playSelection].currentTime = 0;
+        this.options.audioTags[playSelection].play();
         
         return false;
-    };
+    }
     /*
         Replays last x messages
     */
-    this.replay = function(replayCount) {
-        var jsonRpc = getCleanRpcObject('request');
+    , "replay": function(replayCount) {
+        var jsonRpc = this.getRpcObject('request');
         jsonRpc.method = 'requestReplay';
         jsonRpc.params.replayCount = replayCount;
-        WebSock.objWebsocket.send(JSON.stringify(jsonRpc));
+        SockLib.socket.send(JSON.stringify(jsonRpc));
         return false;
-    };
+    }
     /*
         Automatically executes a keep alive to make sure the connection doesn't die.
     */
-    this.keepItAlive = function() {
-        var objJsonRpc = getCleanRpcObject('request');
-        objJsonRpc.method = 'keepAlive';
+    , "keepItAlive": function() {
+        var jsonRpc = this.getRpcObject('request');
+        jsonRpc.method = 'keepAlive';
         try {
-            WebSock.objWebsocket.send(JSON.stringify(objJsonRpc));
+            SockLib.socket.send(JSON.stringify(jsonRpc));
         }
-        catch (objError) {
-            
-            this.objError.code = "" + objError.code;
-            this.objError.message = objError.name;
-            this.objError.data = objError.stack;
+        catch (error) {
+            this.options.error.code = "" + error.code;
+            this.options.error.message = error.name;
+            this.options.error.data = error.stack;
         };
-        
-        setTimeout("WebSock.keepItAlive();", 30000);
+        setTimeout("SockLib.keepItAlive();", 30000);
         
         return false;
-    };
+    }
     /*
         Sends a nudge to other clients.
     */
-    this.sendNudge = function() {
-        var objJsonRpc = getCleanRpcObject('request');
-        objJsonRpc.method = 'nudge';
-        
-        WebSock.objWebsocket.send(JSON.stringify(objJsonRpc));
+    , "sendNudge": function() {
+        var jsonRpc = this.getRpcObject('request');
+        jsonRpc.method = 'nudge';
+        SockLib.socket.send(JSON.stringify(jsonRpc));
         
         return false;
-    };
+    }
     /*
         Plays notification when nudges are received.
     */
-    this.nudge = function(objEvent, objParams) {
-        WebSock.playNotifications('nudge');
+    , "nudge": function(eventObject, objParams) {
+        SockLib.playNotifications('nudge');
         
         return false;
-    };
+    }
     /*
         Sets the peer count if received and notifies if a user left or joined.
     */
-    this.setPeerCount = function(objEvent, objParams) {
-        var intTemp = parseInt(objUserCount.innerHTML);
-        var strSoundToPlay = 'connected';
-        if(intTemp > objParams.peerCount) {
-            strSoundToPlay = 'disconnected';
+    , "setPeerCount": function(eventObject, parameters) {
+        var count = parseInt(this.domSelector.userCount.innerHTML);
+        var playSelection = 'connected';
+        if(count > parameters.peerCount) {
+            playSelection = 'disconnected';
         };
-        objUserCount.innerHTML = objParams.peerCount;
-        WebSock.playNotifications(strSoundToPlay);
+        this.domSelector.userCount.innerHTML = parameters.peerCount;
+        SockLib.playNotifications(playSelection);
         
         return false;
-    };
+    }
     /*
         Displays received messages and plays a notification.
     */
-    this.postMessage = function(objEvent, objParams) {
-        WebSock.updateWindow();
-        setText(objParams.name + ': ' + objParams.message);
-        WebSock.playNotifications('notify');
+    , "postMessage": function(eventObject, parameters) {
+        this.updateWindow();
+        this.setText(parameters.name + ': ' + parameters.message);
+        this.playNotifications('notify');
         
         return false;
-    };
-    
+    }
     /*
         Cleans JSON RPC 2.0 objects for new use.
     */
-    var getCleanRpcObject = function(strObjectType) {
-        WebSock.objOptions.intIdCounter += 1;
-        var objRpc2 = {"jsonrpc": "2.0", "id": WebSock.objOptions.intIdCounter};
-        
-        if(strObjectType === 'result') {
-            objRpc2.result = {};
-        } else if(strObjectType === 'error') {
-            objRpc2.error = WebSock.objError;
-        } else if(strObjectType === 'request') {
-            objRpc2.method = '';
-            objRpc2.params = {};
+    , "getRpcObject": function(objectType) {
+        SockLib.options.jsonId += 1;
+        var rpc2 = {};
+        if(this.options.jsonType.hasOwnProperty(objectType)) {
+            rpc2 = new this.options.jsonType[objectType];
         };
         
-        return objRpc2;
-    };
+        return rpc2;
+    }
     /*
         Receives all messages sent from the server.
     */
-    var onMessage = function(objEvent) {
-        var objJson = JSON.parse(objEvent.data);
+    , "onMessage": function(eventObject) {
+        var jsonObject = JSON.parse(eventObject.data);
         
-        if(objJson.hasOwnProperty('method')) {
+        if(jsonObject.hasOwnProperty('method')) {
             // JSON RPC automatic callback.
-            if(WebSock[(objJson.method)] !== undefined) {
-                WebSock[(objJson.method)](objEvent, objJson.params);
+            if(SockLib.hasOwnProperty(jsonObject.method)) {
+                SockLib[(jsonObject.method)](eventObject, jsonObject.params);
             };
         }
-        else if(objJson.hasOwnProperty('result')) {
+        else if(jsonObject.hasOwnProperty('result')) {
             //  Add result logic.
         }
-        else if(objJson.hasOwnProperty('error')) {
+        else if(jsonObject.hasOwnProperty('error')) {
             //  Add error logic.
         };
         
         return false;
-    };
+    }
     /*
         Sends a JSON RPC message of type request.
     */
-    var sendText = function (strRawText) {
-        var objJsonRpc = getCleanRpcObject('request');
-        objJsonRpc.method = 'postMessage';
-        objJsonRpc.params = {
-            "name": WebSock.objOptions.parameters.name
-            , "message": strRawText
+    , "sendText": function (message) {
+        var jsonRpc = this.getRpcObject('request');
+        jsonRpc.method = 'postMessage';
+        jsonRpc.params = {
+            "name": SockLib.options.parameters.name
+            , "message": message
         };
         
-        setText(objJsonRpc.params.name + ': ' + objJsonRpc.params.message);
+        this.setText(jsonRpc.params.name + ': ' + jsonRpc.params.message);
         
-        WebSock.objWebsocket.send(JSON.stringify(objJsonRpc));
+        SockLib.socket.send(JSON.stringify(jsonRpc));
         return false;
-    };
+    }
     /*
         Parses and inserts text on the page.
     */
-    var setText = function(strMessage) {
-        var objParagraph = document.createElement('p');
-        strMessage = strMessage.replace(/\S+(youtube)\S+v=([a-zA-Z0-9_\-]+)\S*/gim, '<iframe style="youtubeFrame" width="320" height="320" src="//www.youtube.com/embed/$2" frameborder="0" allowfullscreen></iframe>');
-        strMessage = strMessage.replace(/!!(\w+)!!/gim, '<img class="images" src="$1" />');
-        strMessage = strMessage.replace(/\s(http[s]?:\S+)/gim, ' <a href="$1" target="_blank">$1</a>');
-        objParagraph.innerHTML = strMessage;
+    , "setText": function(message) {
+        var domParagraph = document.createElement('p');
+        for(var i = 0; i < this.stringParse.length; i += 1) {
+            var parse = this.stringParse[i];
+            message = message.replace(parse.regex, parse.replace);
+        };
+        domParagraph.innerHTML = message;
         
-        if(objOutput.hasChildNodes()) {
-            objOutput.insertBefore(objParagraph, objOutput.childNodes[0]);
+        if(SockLib.domSelector.chatMessageDisplay.hasChildNodes()) {
+            SockLib.domSelector.chatMessageDisplay.insertBefore(domParagraph, this.domSelector.chatMessageDisplay.childNodes[0]);
         }
         else {
-            objOutput.appendChild(objParagraph);
+            SockLib.domSelector.chatMessageDisplay.appendChild(domParagraph);
         };
         
         return false;
-    };
+    }
     /*
         Executes if an error in the socket is received.
     */
-    var onError = function(objEvent) {
-        objUserCount.innerHTML = 'Error: ' + objEvent.type;
-        console.log(objEvent);
+    , "onError": function(eventObject) {
+        SockLib.setText('Error: ' + eventObject.type);
+        
         return false;
-    };
+    }
     /*
         Executes when a socket connection is established.
     */
-    var onOpen = function() {
-        setText('!Connected to socket!');
+    , "onOpen": function() {
+        this.setText('!Connected to socket!');
         return false;
-    };
+    }
 };
 // Initiates the websocket library.
-WebSock.__constructor();
+SockLib.__constructor();
